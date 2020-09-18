bf6fa6972b04aef8c5ff85d32e0b67b9ec4ff720
David Bango <ybangodavid@gmail.com>
2020-09-18 13:28:58 +0200

    Initial commit

---
+++ teamsstartup.ps1
@@ -0,0 +1,18 @@
+﻿$appName= "Teams"
+$date = Get-Date
+$day = $date.DayOfWeek
+$daysYouDontWorkExpr = $day.Equals([System.DayOfWeek]::Saturday) -or $day.Equals([System.DayOfWeek]::Sunday)
+$hour = $date.TimeOfDay.Hours
+$hourYouStartWorking = 6
+$hourYouStopWorking = 18
+
+
+if ($daysYouDontWorkExpr) {
+    Write-Output "Today is $day, no need to start Teams :)"
+} elseif ($hour -le $hourYouStartWorking -or $hour -ge $hourYouStopWorking) {
+    Write-Output "It's EOB / too early, no need to start Teams :)"
+} else {
+    Write-Output "It's business hours on a weekday, starting Teams..."
+    $userprofile = $env:USERPROFILE
+    Start-Process -FilePath "$userprofile\AppData\Local\Microsoft\Teams\Update.exe" -ArgumentList "--processStart `"Teams.exe`""
+}
